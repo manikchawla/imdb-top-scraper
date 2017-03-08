5eb4685c29ffcd30fed9b750dda4f31604a0385e
Manik Chawla <manik.chawla9@gmail.com>
2017-03-09 01:36:40 +0530

    fixed indentation

--- series.php
+++ series.php
@@ -1,42 +1,33 @@
 <?php
-
 if ($argc != 2) {
-	echo "Invalid number of arguments.";
-	exit();
+    echo "Invalid number of arguments.";
+    exit();
 }
-
 $user_input = $argv[1];
-
 if (preg_match('/^[A-Z]$/', $user_input))
     $result = letter_to_number(ord($user_input) - 64);
 else if (preg_match('/^[0-9]+$/', $user_input))
-  $result = is_numeric(number_to_letter($user_input)) ? chr(64 + number_to_letter($user_input)) : number_to_letter($user_input);
+    $result = is_numeric(number_to_letter($user_input)) ? chr(64 + number_to_letter($user_input)) : number_to_letter($user_input);
 else 
     $result = "Please enter an uppercase letter or number.";
   
 echo $result;
 
 function letter_to_number($n) {
-  if ($n== 1)
-      return 1;
-  else
-      return letter_to_number($n-1) * 2 + $n;
+    if ($n== 1)
+        return 1;
+    else
+        return letter_to_number($n-1) * 2 + $n;
 }
 
 function number_to_letter($n) {
-  for ($i = 1; $i <= 26; $i++) {
-      $answer = letter_to_number($i);
-      if ($answer == $n)
-              return $i;
-  }
-  $i = "This number doesn't occur in the series.";
-  return $i;
+    for ($i = 1; $i <= 26; $i++) {
+        $answer = letter_to_number($i);
+        if ($answer == $n)
+            return $i;
+    }
+    $i = "This number doesn't occur in the series.";
+    return $i;
 }
    
-?>    
-
-
-
-     
-
-
+?> 
